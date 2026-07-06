fc9b128ef123b0af0159d68a2bdb4ed408b67809
Frontenddeveloper2023 <Jayamweb.designer2@gmail.com>
2026-07-06 14:13:50 +0530

    gg

--- src/routes/couponRoutes.js
+++ src/routes/couponRoutes.js
@@ -5,6 +5,8 @@ const { protectAdmin } = require('../middleware/auth');
 
 router.post('/create', protectAdmin, couponController.createCoupon);
 router.get('/getAll', couponController.getAllCoupons);
+router.get('/available', couponController.getAvailableCoupons);
+
 router.put('/toggle-status/:id', protectAdmin, couponController.toggleCouponStatus);
 router.delete('/delete/:id', protectAdmin, couponController.deleteCoupon);
 router.post('/apply', couponController.applyCoupon);
